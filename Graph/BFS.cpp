226d16b0700cf01032debba065878d7831706cb5
Bhavin <bhavin6431@gmail.com>
2021-11-18 09:22:13 +1100

    Update BFS.cpp

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -7,6 +7,8 @@ int main() {
 	m = n - 1;
 	vector<vector<int>> adj(n + 1);
 	vector<bool> visited(n+1, false);
+	vector<int> d(n + 1);
+	vector<int> p(n + 1, -1);
 	for(int i=0;i<m;i++) {
 		cin >> u;
 		cin >> v;
@@ -26,9 +28,25 @@ int main() {
 					c++;
 				}
 				q.push({ne, u.second + 1});
+				d[ne] = d[u.first] + 1;
+				p[ne] = u.first;
 			}
 		}
 	}
-	cout << c;
+	cout << c << endl;
+
+	for(int j = 1; j <= n;j++) {
+		x = j;
+		vector<int> path;
+		path.push_back(x);
+		while(p[x] != - 1) {
+			path.push_back(p[x]);
+			x = p[x];
+		}
+		for(int i=0;i<path.size();i++) {
+			cout << path[i] <<(i != path.size() - 1 ? " -> " : " ");
+		}
+		cout << endl;
+	}
 	return 0;
 }
