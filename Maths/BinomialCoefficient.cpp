db31cb9d68b0b6eb4399e635ac34c6cd99684c9b
Bhavin <bhavin6431@gmail.com>
2021-12-15 12:09:55 +1100

    Create BinomialCoefficient.cpp

---
+++ Maths/BinomialCoefficient.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int MAXN = 10;
+int MOD = 1e9 + 7;
+
+long long calculateFactorial(vector<long long> &factorial) {
+	factorial[0] = 1;
+	for (int i = 1; i < MAXN; i++) {
+		factorial[i] = factorial[i - 1] * i % MOD;
+	}
+}
+
+long long binomial_coefficient(vector<long long> &factorial, vector<long long> &inverse_factorial, int n, int k) {
+	return factorial[n] * inverse_factorial[k] % MOD * inverse_factorial[n - k] % MOD;
+}
+
+void moduleInverse(vector<long long> &inv) {
+	inv[1] = 1;
+	for(int i = 2; i < MAXN; ++i)
+		inv[i] = MOD - (MOD/i) * inv[MOD%i] % MOD;
+
+}
+
+int main() {
+	vector<long long> inv(MAXN, 1);
+	vector<long long> factorial(MAXN, 1);
+	calculateFactorial(factorial);
+	moduleInverse(inv);
+	cout << factorial[5] << endl;
+	cout << inv[5] << endl;
+	cout << binomial_coefficient(factorial, inv, 4 , 2);
+	return 0;
+}
